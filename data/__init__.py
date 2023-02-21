d3d0b5168b679a328f8628008690b7d7673f713a
Ke Zhang <44878027+BWGZK@users.noreply.github.com>
2023-02-21 13:56:34 +0800

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,7 @@
+from .load_data import build
 
+
+def build_dataset(image_set, args):
+    if args.dataset == 'MSCMR':
+        return build(image_set, args)
+    raise ValueError(f'dataset {args.dataset} not supported')
